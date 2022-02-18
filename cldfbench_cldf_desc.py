2c1f334894b51895c6673b42fa24c52e86382413
Florian Matter <florianmatter@gmail.com>
2022-02-18 08:28:22 +0100

    using slices across the board

--- cldfbench_cldf_desc.py
+++ cldfbench_cldf_desc.py
@@ -30,151 +30,113 @@ class Dataset(BaseDataset):
 
         >>> args.writer.objects['LanguageTable'].append(...)
         """
-        morphemes = pd.read_csv("raw/morphemes.csv", keep_default_na=False)
-        morphs = pd.read_csv("raw/morphs.csv", keep_default_na=False)
-        complex_forms = pd.read_csv("raw/complex_forms.csv", keep_default_na=False)
-        examples = pd.read_csv("raw/examples.csv", keep_default_na=False)
+        # morphemes = pd.read_csv("raw/morphemes.csv", keep_default_na=False)
+        # morphs = pd.read_csv("raw/morphs.csv", keep_default_na=False)
+        # complex_forms = pd.read_csv("raw/complex_forms.csv", keep_default_na=False)
+        # examples = pd.read_csv("raw/examples.csv", keep_default_na=False)
 
-        tokenizer = Tokenizer()
+        # tokenizer = Tokenizer()
 
-        def tokenize(string):
-            string = string.replace("-", "")
-            return tokenizer(string)
+        # def tokenize(string):
+        #     string = string.replace("-", "")
+        #     return tokenizer(string)
 
-        morph_links = []
-        complex_forms.drop(columns=["POS"], inplace=True)
-        complex_forms["Language_ID"] = "tri"
-        complex_forms.rename(
-            columns={
-                "Meaning": "Parameter_ID",
-                "Form": "Morph_Segments",
-            },
-            inplace=True,
-        )
-        complex_forms["Form"] = complex_forms["Morph_Segments"].str.replace("+", "")
-        for c in ["Morph_Segments", "Morphs"]:
-            complex_forms[c] = complex_forms[c].apply(
-                lambda x: ch.split_cldf_row(x, sep="+")
-            )
-        for i, row in complex_forms.iterrows():
-            for segslice, (morph_id, string) in enumerate(
-                zip(row["Morphs"], row["Morph_Segments"])
-            ):
-                morph_links.append(
-                    {
-                        "ID": f"{i}-{segslice}",
-                        "Form_ID": row["ID"],
-                        "Morph_ID": morph_id,
-                        "Segment_Slice": str(segslice),
-                    }
-                )
+        # morph_links = []
+        # complex_forms.drop(columns=["POS"], inplace=True)
+        # complex_forms["Language_ID"] = "tri"
+        # complex_forms.rename(
+        #     columns={
+        #         "Meaning": "Parameter_ID",
+        #         "Form": "Morph_Segments",
+        #     },
+        #     inplace=True,
+        # )
+        # complex_forms["Form"] = complex_forms["Morph_Segments"].str.replace("+", "")
+        # for c in ["Morph_Segments", "Morphs"]:
+        #     complex_forms[c] = complex_forms[c].apply(
+        #         lambda x: ch.split_cldf_row(x, sep="+")
+        #     )
+        # for i, row in complex_forms.iterrows():
+        #     for segslice, (morph_id, string) in enumerate(
+        #         zip(row["Morphs"], row["Morph_Segments"])
+        #     ):
+        #         morph_links.append(
+        #             {
+        #                 "ID": f"{i}-{segslice}",
+        #                 "Form_ID": row["ID"],
+        #                 "Morph_ID": morph_id,
+        #                 "Segment_Slice": str(segslice),
+        #             }
+        #         )
 
-        morph_links = pd.DataFrame.from_dict(morph_links)
+        # morph_links = pd.DataFrame.from_dict(morph_links)
 
-        complex_forms["Sound_Segments"] = complex_forms["Form"].apply(tokenize)
-        complex_forms["Sound_Segments"] = complex_forms["Sound_Segments"].apply(
-            lambda x: ch.split_cldf_row(x, " ")
-        )
-        complex_forms.drop(columns=["Morphs"], inplace=True)
+        # complex_forms["Sound_Segments"] = complex_forms["Form"].apply(tokenize)
+        # complex_forms["Sound_Segments"] = complex_forms["Sound_Segments"].apply(
+        #     lambda x: ch.split_cldf_row(x, " ")
+        # )
+        # complex_forms.drop(columns=["Morphs"], inplace=True)
 
-        args.writer.cldf.add_component("FormTable")
-        args.writer.cldf.add_component("ExampleTable")
-        args.writer.cldf.add_component(
-            {
-                "url": "form_morphs.csv",
-                "tableSchema": {
-                    "columns": [
-                        {
-                            "name": "ID",
-                            "required": True,
-                            "propertyUrl": "http://cldf.clld.org/v1.0/terms.rdf#id",
-                            "datatype": {
-                                "base": "string",
-                                "format": "[a-zA-Z0-9_\\-]+",
-                            },
-                        },
-                        {
-                            "name": "Form_ID",
-                            "required": True,
-                            "dc:extent": "singlevalued",
-                            "datatype": "string",
-                        },
-                        {
-                            "name": "Morph_ID",
-                            "required": True,
-                            "dc:extent": "singlevalued",
-                            "datatype": "string",
-                        },
-                        {
-                            "name": "Segment_Slice",
-                            "required": False,
-                            "propertyUrl": "http://cldf.clld.org/v1.0/terms.rdf#segmentSlice",
-                            "dc:description": "Specifies the slice of morphemes.",
-                            "datatype": {
-                                "base": "string",
-                                "format": "\\d+(:\\d+)?",
-                            },
-                            "separator": " ",
-                        },
-                    ]
-                },
-            }
-        )
-        args.writer.cldf.add_component(
-            component=jsonlib.load("etc/MorphTable-metadata.json")
-        )
-        args.writer.cldf.add_component(
-            component=jsonlib.load("etc/MorphsetTable-metadata.json")
-        )
-        args.writer.cldf.remove_columns("FormTable", "Source", "Segments")
-        args.writer.cldf.add_columns("ExampleTable", {
-                "name": "Morpheme_IDs",
-                "required": False,
-                "dc:extent": "multivalued",
-                "datatype": "string",
-                "separator": "; "
-            })
-        args.writer.cldf.add_columns("FormTable", {
-                "name": "Morph_Segments",
-                "required": False,
-                "dc:extent": "multivalued",
-                "datatype": "string",
-                "separator": " "
-            }, {
-                "name": "Sound_Segments",
-                "required": False,
-                "dc:extent": "multivalued",
-                "datatype": "string",
-                "separator": " "
-            })
+        # args.writer.cldf.add_component("FormTable")
+        # args.writer.cldf.add_component("ExampleTable")
+        # args.writer.cldf.add_component(
+        #     jsonlib.load("etc/FormSlices-metadata.json")
+        # )
+        # args.writer.cldf.add_component(
+        #     component=jsonlib.load("etc/MorphTable-metadata.json")
+        # )
+        # args.writer.cldf.add_component(
+        #     component=jsonlib.load("etc/MorphsetTable-metadata.json")
+        # )
+        # args.writer.cldf.remove_columns("FormTable", "Source", "Segments")
+        # args.writer.cldf.add_columns("ExampleTable", {
+        #         "name": "Morpheme_IDs",
+        #         "required": False,
+        #         "dc:extent": "multivalued",
+        #         "datatype": "string",
+        #         "separator": "; "
+        #     })
+        # args.writer.cldf.add_columns("FormTable", {
+        #         "name": "Morph_Segments",
+        #         "required": False,
+        #         "dc:extent": "multivalued",
+        #         "datatype": "string",
+        #         "separator": " "
+        #     }, {
+        #         "name": "Sound_Segments",
+        #         "required": False,
+        #         "dc:extent": "multivalued",
+        #         "datatype": "string",
+        #         "separator": " "
+        #     })
 
-        args.writer.cldf.add_foreign_key(
-            "morphs.csv", "Morpheme_ID", "morphemes.csv", "ID"
-        )
-        args.writer.cldf.add_foreign_key(
-            "ExampleTable", "Morpheme_IDs", "morphemes.csv", "ID"
-        )
-        args.writer.cldf.add_foreign_key(
-            "form_morphs.csv", "Form_ID", "FormTable", "ID"
-        )
-        args.writer.cldf.add_foreign_key(
-            "form_morphs.csv", "Morph_ID", "morphs.csv", "ID"
-        )
+        # args.writer.cldf.add_foreign_key(
+        #     "morphs.csv", "Morpheme_ID", "morphemes.csv", "ID"
+        # )
+        # args.writer.cldf.add_foreign_key(
+        #     "ExampleTable", "Morpheme_IDs", "morphemes.csv", "ID"
+        # )
+        # args.writer.cldf.add_foreign_key(
+        #     "form_morphs.csv", "Form_ID", "FormTable", "ID"
+        # )
+        # args.writer.cldf.add_foreign_key(
+        #     "form_morphs.csv", "Morph_ID", "morphs.csv", "ID"
+        # )
 
-        examples["Analyzed_Word"] = examples["Analyzed_Word"].str.split("\t")
-        examples["Gloss"] = examples["Gloss"].str.split("\t")
-        examples["Morpheme_IDs"] = examples["Morpheme_IDs"].str.split("; ")
-        morphs["Segments"] = morphs["Segments"].str.split(" ")
+        # examples["Analyzed_Word"] = examples["Analyzed_Word"].str.split("\t")
+        # examples["Gloss"] = examples["Gloss"].str.split("\t")
+        # examples["Morpheme_IDs"] = examples["Morpheme_IDs"].str.split("; ")
+        # morphs["Segments"] = morphs["Segments"].str.split(" ")
         
-        for i, e in morph_links.iterrows():
-            args.writer.objects["form_morphs.csv"].append(e)
-        for i, morpheme in morphemes.iterrows():
-            args.writer.objects["morphemes.csv"].append(morpheme)
-        for i, form in morphs.iterrows():
-            args.writer.objects["morphs.csv"].append(form)
-        for i, ex in examples.iterrows():
-            args.writer.objects["ExampleTable"].append(ex)
-        for i, form in complex_forms.iterrows():
-            args.writer.objects["FormTable"].append(form)
+        # for i, e in morph_links.iterrows():
+        #     args.writer.objects["form_morphs.csv"].append(e)
+        # for i, morpheme in morphemes.iterrows():
+        #     args.writer.objects["morphemes.csv"].append(morpheme)
+        # for i, form in morphs.iterrows():
+        #     args.writer.objects["morphs.csv"].append(form)
+        # for i, ex in examples.iterrows():
+        #     args.writer.objects["ExampleTable"].append(ex)
+        # for i, form in complex_forms.iterrows():
+        #     args.writer.objects["FormTable"].append(form)
 
-        args.writer.write()
+        # args.writer.write()
